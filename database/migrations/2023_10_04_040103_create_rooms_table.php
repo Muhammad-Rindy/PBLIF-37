a18c4ab7302d5dd9e6328782e1fc4028eaa752f7
Muhammad-Rindy <muhammadrindy2705@gmail.com>
2023-10-06 19:14:39 +0700

    commit

--- database/migrations/2023_10_04_040103_create_rooms_table.php
+++ database/migrations/2023_10_04_040103_create_rooms_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('room');
             $table->string('edifice');
             $table->integer('level');
-            $table->timestamps();
         });
     }
 
